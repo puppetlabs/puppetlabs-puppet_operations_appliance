0f7836a142c55a9b50f3d2a4e71ee1a365986aae
martyewings <martyewings@gmail.com>
2020-11-23 13:59:36 +0000

    comments for needed work in each class

--- manifests/exporter.pp
+++ manifests/exporter.pp
@@ -1,8 +1,47 @@
-# @summary A short summary of the purpose of this class
 #
-# A description of what this class does
-#
+# When Applied to the Infrastruture Agent Node group, Will dynamically configure all matching nodes to allow access to key elements of Puppet Enterprise to the RSAN node
+# 
 # @example
 #   include rsan::exporter
 class rsan::exporter {
+
+
+
+########################1.  Export Logging Function######################
+# Need to determine automatically the Network Fact IP for the RSAN::importer node automatically, applies to all infrastructure nodes
+#########################################################################
+
+
+
+
+######################2. Metrics Dash Board deployment ###############
+# Assuming use of puppet metrics dashboard for telemetry all nodes need
+# include puppet_metrics_dashboard::profile::master::install
+###################################################################
+
+
+
+#####################3. Metrics Dashboard postgres access ############
+# Determine if node is pe_postgres host and conditionally apply include puppet_metrics_dashboard::profile::master::postgres_access
+######################################################################
+
+
+
+
+
+#####################3. RSANpostgres command access ######################
+# Determine if node is pe_postgres host and conditionally apply Select Access for the RSAN node cert to all PE databases
+# Hint metrics dashboard postgres access code can be duplicated and repurposed
+######################################################################
+
+
+
+
+
+
+
+
+
+
+
 }
--- manifests/importer.pp
+++ manifests/importer.pp
@@ -1,8 +1,51 @@
-# @summary A short summary of the purpose of this class
-#
-# A description of what this class does
-#
+# Class to consume the resources provided by the exporter class.
+# when applied to a node, all tooling agttributed to RSAN will be set up
 # @example
 #   include rsan::importer
 class rsan::importer {
+
+
+
+  ##################### 1.Import logging from the exporter groups #####################
+  # depending on the method, could be import exported respore with rsan tag
+  #####################################################################################
+
+
+
+  #################### 2. Deploy Client tools, and deploy PSL client #################
+  # include postgresql::client , include puppet_enterprise::profile::controller need to make postgresql module a dependancies
+  ####################################################################################
+
+
+  ################### 3. Telemetry dashboard ########################################
+  # If using puppet_metrics_dashboard:
+
+    #   class { 'puppet_metrics_dashboard':
+      # add_dashboard_examples => true,
+      # overwrite_dashboards   => false,
+      # configure_telegraf     => true,
+      #enable_telegraf        => true,
+      #master_list            => $infranode,
+      #puppetdb_list          => [$pdb],
+      #postgres_host_list     => [$postgres],
+
+      #}
+
+      # master_list , puppetdb_list, postgres_host_list need to be queried  from the system programatically
+
+
+
+  #######################################################################################
+
+
+  ##################### 4. VPN client (openvpn) ########################################
+  # deploy openvpn client, set up connection with preshared key use licence key UUID as preshared key
+  # destination will need IT involvement, scope to make it possible with a dummy end point
+  # Task to enable and disable connection
+  ######################################################################################
+
+
+
+
+
 }
