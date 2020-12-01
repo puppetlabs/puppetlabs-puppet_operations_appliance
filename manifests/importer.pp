de6946161b486b366bbc4f93684731aaeae52b3a
Liam Sexton <liam.sexton@Liams-MBP-2.temple.local>
2020-12-01 17:53:37 +0000

    added function to search for db hosts

--- manifests/importer.pp
+++ manifests/importer.pp
@@ -24,6 +24,24 @@ class rsan::importer {
   # this is where lists of master_list , puppetdb_list, postgres_host_list hosts are found using a puppetdb query
   #
 
+  function puppet_enterprise::active_puppetdb_hosts() {
+    if $settings::storeconfigs {
+      $active_puppetdb_hosts =
+                  puppetdb_query('resources[certname] {
+                      type = "Class" and
+                      title = "Puppet_enterprise::Profile::Puppetdb" and
+                      nodes {
+                        deactivated is null and
+                        expired is null
+                      }
+                    }').map |$data| { $data['certname'] }
+    } else {
+      $active_puppetdb_hosts = []
+  }
+
+  pe_sort($active_puppetdb_hosts)
+}
+
 
   #make sure each list is same format
 
