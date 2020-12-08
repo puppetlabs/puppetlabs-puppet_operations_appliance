d9e9c5baad85c2ecc7f84defc64032c331f4d0c0
Liam Sexton <liam.sexton@Liams-MBP-2.temple.local>
2020-12-08 11:06:45 +0000

    added function to query for puppet servers

--- functions/get_puppet_servers.pp
+++ functions/get_puppet_servers.pp
@@ -1,11 +1,11 @@
 function rsan::get_puppet_servers() {
   $puppet_servers =
-           puppetdb_query("nodes[certname] {
+          puppetdb_query("nodes[certname] {
                      resources {
                          type = 'Class' and
                          title = 'Puppet_enterprise::Profile::Master'
                      } and deactivated is null and expired is null }").map |$node| {
                         $node['certname']
-                     }
+                    }
   pe_sort($puppet_servers)
 }
