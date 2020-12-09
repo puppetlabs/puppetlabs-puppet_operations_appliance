7ca726dd57ff3030483a7e3bfc975f2453a742c2
Liam Sexton <liam.sexton@Liams-MBP-2.temple.local>
2020-12-09 11:26:27 +0000

    changed ' to " to be safe in pdbquery

--- functions/get_puppet_servers.pp
+++ functions/get_puppet_servers.pp
@@ -1,10 +1,10 @@
 function rsan::get_puppet_servers() {
   $puppet_servers =
-          puppetdb_query("nodes[certname] {
+          puppetdb_query('nodes[certname] {
                      resources {
-                         type = 'Class' and
-                         title = 'Puppet_enterprise::Profile::Master'
-                     } and deactivated is null and expired is null }").map |$node| {
+                         type = "Class" and
+                         title = "Puppet_enterprise::Profile::Master"
+                     } and deactivated is null and expired is null }').map |$node| {
                         $node['certname']
                     }
   pe_sort($puppet_servers)
