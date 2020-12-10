6ebf3e45d941b886ef9dd3a5e90c9cc16f0c2fa6
Liam Sexton <liam.sexton@Liams-MBP-2.temple.local>
2020-12-10 10:38:33 +0000

    changed the class title to be in capital letters

--- functions/get_postgres_hosts.pp
+++ functions/get_postgres_hosts.pp
@@ -1,18 +1,15 @@
 function rsan::get_postgres_hosts() {
-  if $settings::storeconfigs {
     $postgres_hosts =
                 puppetdb_query('resources[certname] {
                     type = "Class" and
-                    title = "pe_postgresql::server::install" and
+                    title = "Pe_postgresql::Server::Install" and
                     nodes {
                       deactivated is null and
                       expired is null
                     }
                   }').map |$data| { $data['certname'] }
-  } else {
-    $postgres_hosts = []
   }
 
   pe_sort($postgres_hosts)
 
-}
+
