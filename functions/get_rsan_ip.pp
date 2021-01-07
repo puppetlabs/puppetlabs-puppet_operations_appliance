cffad87c6fc8b8730d4036b5a86401af0299ea6b
chris lawrence <chris.lawrence@puppet.com>
2021-01-07 16:32:11 +0000

    adding tags to rsan importer

--- functions/get_rsan_ip.pp
+++ functions/get_rsan_ip.pp
@@ -1,18 +1,18 @@
 function rsan::get_rsan_ip() {
   if $settings::storeconfigs {
     $rsan_ip =
-      puppetdb_query('facts[value]{ 
-        name = "ipaddress" and 
-        certname in resources[certname] { 
-          type = "Class" and 
-          title = "Rsan::Importer" and 
-          nodes { 
-            deactivated is null and 
+      puppetdb_query('facts[value]{
+        name = "ipaddress" and
+        certname in resources[certname] {
+          type = "Class" and
+          title = "Rsan::Importer" and
+          nodes {
+            deactivated is null and
             expired is null
             }
           }
-        }').map |$data| { $data[value] }
+        }')[0][value]
   } else {
-    $rsan_ip = []
+    $rsan_ip = Undef
   }
 }
