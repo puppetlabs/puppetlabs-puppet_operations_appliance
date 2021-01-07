65c85e873cdb49f6c890312c5d1403ee84928c93
chris lawrence <chris.lawrence@puppet.com>
2021-01-07 12:17:51 +0000

    updating network to ip parameter

--- lib/puppet/functions/get_rsan_ip.pp
+++ lib/puppet/functions/get_rsan_ip.pp
@@ -1,14 +1,14 @@
 function Functions::get_rsan_ip() {
   if $settings::storeconfigs {
     $rsan_ip =
-                puppetdb_query('resources[network] {
+                puppetdb_query('resources[ipaddress] {
                     type = "Class" and
                     title = "Rsan::Importer" and
                     nodes {
                       deactivated is null and
                       expired is null
                     }
-                  }').map |$data| { $data['network'] }
+                  }').map |$data| { $data['ipaddress'] }
   } else {
     $rsan_ip = []
   }
