945a1de36ce0d48391c487da97bdbe08fd7fc789
Morgan Lillie <61731997+morgan-lillie@users.noreply.github.com>
2021-01-07 15:38:22 +0000

    Feature - Live Telemetry display
    * Update exporter.pp
    
    Identifies the presence of postgresql (maybe)
    
    * Update exporter.pp
    
    Added class definition
    
    * Update exporter.pp
    
    * Update exporter.pp
    
    * Testing
    
    * confirm test
    
    * Fixed exporter (maybe)
    
    * test
    
    * new test
    
    * deleted test comments
    
    * testing account
    
    * account test successful, deleting comments
    
    * Removed old comment
    
    * Added 'include puppet_metrics_dashboard::profile::master::install' to step 2 of exporter.pp
    
    * Added content from issue 4
    
    * Added all databases to command access, iterating grant access
    
    * Fixed metadata.json
    
    * Added $rsan_host as parameter and added missing }
    
    * Edited exporter.pp
    
    * changed name for database grant to change with iterations
    
    * Changed whitelist to allow list
    
    * update exporter.pp
    
    * directly call pe_postgresql_psql for table perms
    
    * editied exporter.pp
    
    * edit exporter.pp
    
    * update exporter.pp
    
    * Add require to grant rules
    
    * Include cert_allowlist_entry in iteration
    
    Co-authored-by: SE Demo User <noreply@puppet.com>
    Co-authored-by: Kevin Reeuwijk <kevin.reeuwijk@puppet.com>

--- manifests/exporter.pp
+++ manifests/exporter.pp
@@ -1,47 +1,116 @@
 #
-# When Applied to the Infrastruture Agent Node group, Will dynamically configure all matching nodes to allow access to key elements of Puppet Enterprise to the RSAN node
+# When Applied to the Infrastruture Agent Node group, 
+#Will dynamically configure all matching nodes to allow access to key elements of Puppet Enterprise to the RSAN node
 # 
 # @example
 #   include rsan::exporter
-class rsan::exporter {
+class rsan::exporter(
+  $rsan_host = undef
+) {
 
 
 
-########################1.  Export Logging Function######################
-# Need to determine automatically the Network Fact IP for the RSAN::importer node automatically, applies to all infrastructure nodes
-#########################################################################
-
-
-
-
-######################2. Metrics Dash Board deployment ###############
-# Assuming use of puppet metrics dashboard for telemetry all nodes need
-# include puppet_metrics_dashboard::profile::master::install
-###################################################################
-
-
-
-#####################3. Metrics Dashboard postgres access ############
-# Determine if node is pe_postgres host and conditionally apply include puppet_metrics_dashboard::profile::master::postgres_access
-######################################################################
+  ########################1.  Export Logging Function######################
+  # Need to determine automatically the Network Fact IP for the RSAN::importer node automatically, applies to all infrastructure nodes
+  #########################################################################
 
 
 
 
+  ######################2. Metrics Dash Board deployment ###############
+  # Assuming use of puppet metrics dashboard for telemetry all nodes need
+  # include puppet_metrics_dashboard::profile::master::install
+  ###################################################################
 
-#####################3. RSANpostgres command access ######################
-# Determine if node is pe_postgres host and conditionally apply Select Access for the RSAN node cert to all PE databases
-# Hint metrics dashboard postgres access code can be duplicated and repurposed
-######################################################################
-
-
-
+  include puppet_metrics_dashboard::profile::master::install
 
 
+  #####################3. Metrics Dashboard postgres access ############
+  # Determine if node is pe_postgres host and conditionally apply include puppet_metrics_dashboard::profile::master::postgres_access
+  ######################################################################
+
+  #The following code serves to check that postgres is present and then declares the class
+
+  if $facts['pe_postgresql_info'] != undef {
+    include puppet_metrics_dashboard::profile::master::postgres_access
+  }
 
 
 
+  #####################3. RSANpostgres command access ######################
+  # Determine if node is pe_postgres host and conditionally apply Select Access for the RSAN node cert to all PE databases
+  # Hint metrics dashboard postgres access code can be duplicated and repurposed
+  ######################################################################
 
+  if $facts['pe_postgresql_info'] != undef {
 
+    if $rsan_host {
+      $_rsan_host = $rsan_host
+    } else {
+      $_query = puppetdb_query('resources[certname] {
+        type = "Class" and
+        title = "Rsan::Importer" and             
+        nodes {
+          deactivated is null and
+          expired is null
+        }
+        order by certname asc
+        limit 1
+      }')
+      unless $_query.empty {
+        $_rsan_host = $_query[0]['certname']
+      }
+    }
 
+    # If $rsan_host is not defined and the query fails to find a rsan  host, issue a warning.
+
+    if $_rsan_host == undef {
+
+      notify { 'You must specify rsan_host (or apply the rsan class to an agent) to enable access.': }
+
+    } else {
+
+      pe_postgresql::server::role { 'rsan': }
+
+      if $facts['pe_postgresql_info']['installed_server_version'] {
+        $postgres_version = $facts['pe_postgresql_info']['installed_server_version']
+      } else {
+        $postgres_version = '9.4'
+      }
+
+      $dbs = ['pe-activity', 'pe-classifier', 'pe-inventory', 'pe-puppetdb', 'pe-rbac', 'pe-orchestrator']
+      $dbs.each |$db|{
+        pe_postgresql::server::database_grant { "CONNECT to rsan for ${db}":
+          privilege => 'CONNECT',
+          db        => $db,
+          role      => 'rsan',
+          require   => Pe_postgresql::Server::Role['rsan']
+        }
+
+        $grant_cmd = "GRANT SELECT ON ALL TABLES IN SCHEMA \"public\" TO rsan"
+        pe_postgresql_psql { "${grant_cmd} on ${db}":
+          command    => $grant_cmd,
+          db         => $db,
+          port       => $pe_postgresql::server::port,
+          psql_user  => $pe_postgresql::server::user,
+          psql_group => $pe_postgresql::server::group,
+          psql_path  => $pe_postgresql::server::psql_path,
+          unless     => "SELECT grantee, privilege_type FROM information_schema.role_table_grants WHERE privilege_type = 'SELECT' AND grantee = 'rsan'",
+          require    => [
+            Class['pe_postgresql::server'],
+            Pe_postgresql::Server::Role['rsan']
+          ]
+        }
+
+        puppet_enterprise::pg::cert_allowlist_entry { "allow-rsan-access for ${db}":
+          user                          => 'rsan',
+          database                      => $db,
+          allowed_client_certname       => $_rsan_host,
+          pg_ident_conf_path            => "/opt/puppetlabs/server/data/postgresql/${postgres_version}/data/pg_ident.conf",
+          ip_mask_allow_all_users_ssl   => '0.0.0.0/0',
+          ipv6_mask_allow_all_users_ssl => '::/0',
+        }
+      }
+    }
+  }
 }
