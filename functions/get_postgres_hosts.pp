1f0d2f99e223d7f3e99fc5ac381b612f257cf0b4
liamjohnsexton <61158344+liamjohnsexton@users.noreply.github.com>
2021-01-07 13:12:47 +0000

    Metrics feature
    * uncommented class and added instructions
    
    * added function to search for db hosts
    
    * changed original function variable to $pdb
    
    * reverted to just using class
    
    * functions for getting puppetdb hosts+puppet servers
    
    * created function for querting for postgres hosts
    
    * added function to query for puppet servers
    
    * added function calls, consisten variable names
    
    * added comma to make pdk validate work
    
    * changed ' to " to be safe in pdbquery
    
    * changed the class title to be in capital letters
    
    * added deafult values for the class
    
    * included postgresql::client
    
    * put in correct comment line
    
    * added if criteria
    
    * removed redefining aspect
    
    * tricked you
    
    * making pdk validate happy
    
    * msking pdk validate happy part 2
    
    * asdas
    
    * removing ruby code
    
    * altered syntax
    
    Co-authored-by: Liam Sexton <liam.sexton@Liams-MBP-2.temple.local>
    Co-authored-by: MartyEwings <martyewings@gmail.com>

---
+++ functions/get_postgres_hosts.pp
@@ -0,0 +1,16 @@
+function rsan::get_postgres_hosts() {
+    $postgres_hosts =
+                puppetdb_query('resources[certname] {
+                    type = "Class" and
+                    title = "Pe_postgresql::Server::Install" and
+                    nodes {
+                      deactivated is null and
+                      expired is null
+                    }
+                  }').map |$data| { $data['certname'] }
+
+  pe_sort($postgres_hosts)
+
+}
+
+
--- manifests/importer.pp
+++ manifests/importer.pp
@@ -4,8 +4,6 @@
 #   include rsan::importer
 class rsan::importer {
 
-
-
   ##################### 1.Import logging from the exporter groups #####################
   # depending on the method, could be import exported respore with rsan tag
   #####################################################################################
@@ -16,21 +14,34 @@ class rsan::importer {
   # include postgresql::client , include puppet_enterprise::profile::controller need to make postgresql module a dependancies
   ####################################################################################
 
+  include postgresql::client
   include puppet_enterprise::profile::controller
+
 
   ################### 3. Telemetry dashboard ########################################
   # If using puppet_metrics_dashboard:
 
-    #   class { 'puppet_metrics_dashboard':
-      # add_dashboard_examples => true,
-      # overwrite_dashboards   => false,
-      # configure_telegraf     => true,
-      #enable_telegraf        => true,
-      #master_list            => $infranode,
-      #puppetdb_list          => [$pdb],
-      #postgres_host_list     => [$postgres],
+  #
+  # this is where lists of master_list , puppetdb_list, postgres_host_list hosts are found using a puppetdb query
+  #make sure each list is same format
 
-      #}
+  #conditions of whether postgres is present or not
+
+  #in the below class 
+
+    $puppet_servers = rsan::get_puppet_servers()
+    $puppetdb_hosts = rsan::get_puppetdb_hosts()
+    $postgres_hosts = rsan::get_postgres_hosts()
+
+    class { 'puppet_metrics_dashboard':
+      add_dashboard_examples => true,
+      overwrite_dashboards   => false,
+      configure_telegraf     => true,
+      enable_telegraf        => true,
+      master_list            => $puppet_servers,
+      puppetdb_list          => $puppetdb_hosts,
+      postgres_host_list     => $postgres_hosts,
+    }
 
       # master_list , puppetdb_list, postgres_host_list need to be queried  from the system programatically
 
