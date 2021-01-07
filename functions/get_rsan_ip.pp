82c96d214b46afc425dd2c4674336c68dafd1669
chris lawrence <chris.lawrence@puppet.com>
2021-01-07 15:25:02 +0000

    updating function puppetdb_query

--- functions/get_rsan_ip.pp
+++ functions/get_rsan_ip.pp
@@ -1,14 +1,17 @@
 function rsan::get_rsan_ip() {
   if $settings::storeconfigs {
     $rsan_ip =
-                puppetdb_query('resources[certname] {
-                    type = "Class" and
-                    title = "Rsan::Importer" and
-                    nodes {
-                      deactivated is null and
-                      expired is null
-                    }
-                  }').map |$data| { $data['certname'] }
+      puppetdb_query('facts[value]{ 
+        name = "ipaddress" and 
+        certname in resources[certname] { 
+          type = "Class" and 
+          title = "Rsan::Importer" and 
+          nodes { 
+            deactivated is null and 
+            expired is null
+            }
+          }
+        }').map |$data| { $data['certname'] }
   } else {
     $rsan_ip = []
   }
