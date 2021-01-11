38186d44a0e59427d8ccfdc30c770de730fbac16
Dimitri Tischenko <dimitri@puppet.com>
2021-01-11 16:31:45 +0100

    Fix validate warnings

--- spec/classes/importer_spec.rb
+++ spec/classes/importer_spec.rb
@@ -4,13 +4,14 @@ require 'spec_helper'
 
 describe 'rsan::importer' do
   before :each do
-    Puppet::Parser::Functions.newfunction(:puppetdb_query, :type => :rvalue, :arity => 1) do |args|
+    Puppet::Parser::Functions.newfunction(:puppetdb_query, type: rvalue, arity: 1) do |args|
       []
-    end  
+    end
   end
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+
       it { is_expected.to compile }
     end
   end
