3391bb277e3ddfb67ddfb9e280ff56c7bf1194e8
Jarret Lavallee <jarret.lavallee@puppet.com>
2021-01-11 09:21:21 -0700

    (maint) Stub out puppet_enterprise in spec tests
    Prior to this commit, the module used the real puppet_enterprise module
    to do the spec testing. The puppet_enterprise module is an private git
    repo, so external testing would not work without authorization. This
    commit stubs out the puppet_enterprise module to get the spec tests
    working without github authorization.

--- spec/classes/importer_spec.rb
+++ spec/classes/importer_spec.rb
@@ -6,11 +6,16 @@ describe 'rsan::importer' do
   before :each do
     Puppet::Parser::Functions.newfunction(:puppetdb_query, :type => :rvalue, :arity => 1) do |args|
       []
-    end  
+    end
+    Puppet::Parser::Functions.newfunction(:pe_sort, :type => :rvalue, :arity => 1) do |args|
+      []
+    end
   end
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:pre_condition) { 'class puppet_enterprise::profile::controller {}' }
+
       it { is_expected.to compile }
     end
   end
--- spec/spec_helper_local.rb
+++
@@ -1,3 +0,0 @@
-RSpec.configure do |c|
-  c.hiera_config = File.expand_path(File.join(__dir__, 'fixtures/hiera.yaml'))
-end
