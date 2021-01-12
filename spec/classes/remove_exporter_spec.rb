52ea799c33b6d14bb04264fbc2ac604a70ebb246
MartyEwings <martyewings@gmail.com>
2021-01-12 14:56:40 +0000

    remove exporter class state from infra nodes (#36)
    * add uninstaller
    
    * Update remove_exporter.pp
    
    * add uninstaller
    
    * postgres node detector
    
    * Update remove_exporter.pp
    
    * add uninstaller
    
    * fix style
    
    * fix style

--- spec/classes/remove_exporter_spec.rb
+++ spec/classes/remove_exporter_spec.rb
@@ -2,12 +2,7 @@
 
 require 'spec_helper'
 
-describe 'rsan::importer' do
-  before :each do
-    Puppet::Parser::Functions.newfunction(:puppetdb_query, type: :rvalue, arity: 1) do |_args|
-      []
-    end
-  end
+describe 'rsan::remove_exporter' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
