92029c318a6b1922d869d9003654b74cd9027946
martyewings <martyewings@gmail.com>
2021-01-12 13:10:22 +0000

    fix style

--- manifests/remove_exporter.pp
+++ manifests/remove_exporter.pp
@@ -6,12 +6,7 @@
 #   include rsan::remove_exporter
 class rsan::remove_exporter {
 
-
-#Disable NFS Server
-
-# class { '::nfs':
-  #   server_enabled => false
-  #}
+  #Disable NFS Server and revert config
 
   file { '/etc/exports':
     ensure => absent,
@@ -21,37 +16,33 @@ class rsan::remove_exporter {
     ensure => stopped,
   }
 
-
   if $facts['pe_postgresql_info'] != undef and $facts['pe_postgresql_info']['installed_server_version'] != '' {
 
+    $dbs = ['pe-activity', 'pe-classifier', 'pe-inventory', 'pe-puppetdb', 'pe-rbac', 'pe-orchestrator']
+    $dbs.each |$db|{
 
-  $dbs = ['pe-activity', 'pe-classifier', 'pe-inventory', 'pe-puppetdb', 'pe-rbac', 'pe-orchestrator']
-  $dbs.each |$db|{
+      $dropowned_cmd = 'DROP OWNED BY rsan'
+      pe_postgresql_psql { "${dropowned_cmd} on ${db}":
+      command    => $dropowned_cmd,
+      db         => $db,
+      port       => $pe_postgresql::server::port,
+      psql_user  => $pe_postgresql::server::user,
+      psql_group => $pe_postgresql::server::group,
+      psql_path  => $pe_postgresql::server::psql_path,
+      require    => [Class['pe_postgresql::server']]
+    }
 
-  $dropowned_cmd = 'DROP OWNED BY rsan'
-        pe_postgresql_psql { "${dropowned_cmd} on ${db}":
-          command    => $dropowned_cmd,
-          db         => $db,
-          port       => $pe_postgresql::server::port,
-          psql_user  => $pe_postgresql::server::user,
-          psql_group => $pe_postgresql::server::group,
-          psql_path  => $pe_postgresql::server::psql_path,
-          require    => [Class['pe_postgresql::server']]
-        }
+  }
 
-      }
-
-
-      $droprole_cmd = 'DROP ROLE rsan'
-        pe_postgresql_psql { "${droprole_cmd}  ":
-          command    => $droprole_cmd,
-          db         => pe-puppetdb,
-          port       => $pe_postgresql::server::port,
-          psql_user  => $pe_postgresql::server::user,
-          psql_group => $pe_postgresql::server::group,
-          psql_path  => $pe_postgresql::server::psql_path,
-          require    => Pe_postgresql_psql['DROP OWNED BY rsan on pe-puppetdb'],
-        }
-
+    $droprole_cmd = 'DROP ROLE rsan'
+    pe_postgresql_psql { "${droprole_cmd}  ":
+      command    => $droprole_cmd,
+      db         => pe-puppetdb,
+      port       => $pe_postgresql::server::port,
+      psql_user  => $pe_postgresql::server::user,
+      psql_group => $pe_postgresql::server::group,
+      psql_path  => $pe_postgresql::server::psql_path,
+      require    => Pe_postgresql_psql['DROP OWNED BY rsan on pe-puppetdb'],
+    }
   }
 }
