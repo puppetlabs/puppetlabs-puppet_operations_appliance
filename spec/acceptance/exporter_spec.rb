f810fdbeabe7b871bbf9a5a49b2fe1f78e885973
martyewings <martyewings@gmail.com>
2021-06-14 16:45:53 +0100

    fixup spec helper

--- spec/acceptance/exporter_spec.rb
+++ spec/acceptance/exporter_spec.rb
@@ -3,14 +3,13 @@ require 'spec_helper_acceptance'
 describe 'Exporter class' do
   context 'activates module default parameters' do
     it 'applies the class with default parameters' do
-      pp = <<-MANIFEST
-        include rsan::exporter
-        MANIFEST
+      run_shell('/bin/echo \"File \{ backup => false \}
+node default \{
+  # This is where you can declare classes for all nodes.
+  class \{ \'rsan:exporter\': \}" > test.pp')
 
-      # Run it twice and test for idempotency
-      expect(apply_manifest(pp).exit_code).not_to eq(1)
-      expect(apply_manifest(pp).exit_code).not_to eq(1)
-      idempotent_apply(pp)
+      expect(run_shell('/opt/puppetlabs/bin/puppet agent -t').exit_code).not_to eq(1)
+      expect(run_shell('/opt/puppetlabs/bin/puppet agent -t').exit_code).not_to eq(1)
     end
   end
 end
--- spec/acceptance/support_user_spec.rb
+++ spec/acceptance/support_user_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper_acceptance'
 describe 'tasks' do
   it 'task not to fail and password to be created ' do
     result = run_bolt_task('rsan::supportuser')
-    expect(result['result']['_output']).to contain('created')
+    expect(result['result']['_output']).to match('created')
   end
 end
