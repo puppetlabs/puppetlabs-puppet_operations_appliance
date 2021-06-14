d3ad1e640ee13858d72c249dbe81f59eeca9f065
martyewings <martyewings@gmail.com>
2021-06-14 17:01:00 +0100

    fixup spec helper

--- spec/acceptance/exporter_spec.rb
+++ spec/acceptance/exporter_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper_acceptance'
 describe 'Exporter class' do
   context 'activates module default parameters' do
     it 'applies the class with default parameters' do
-      run_shell('/bin/echo \"File \{ backup => false \}
-node default \{
+      run_shell('/bin/echo "File { backup => false }
+node default {
   # This is where you can declare classes for all nodes.
-  class \{ \'rsan:exporter\': \}" > /etc/puppetlabs/code/environments/production/manifests/site.pp')
+  class { \'rsan:exporter\': }" > /etc/puppetlabs/code/environments/production/manifests/site.pp')
 
       expect(run_shell('/opt/puppetlabs/bin/puppet agent -t').exit_code).not_to eq(1)
       expect(run_shell('/opt/puppetlabs/bin/puppet agent -t').exit_code).not_to eq(1)
