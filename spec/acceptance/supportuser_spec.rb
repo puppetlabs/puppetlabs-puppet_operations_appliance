9f7618473dd123df9cc168026629bacbc9e1d653
martyewings <martyewings@gmail.com>
2021-06-15 11:42:32 +0100

    fixup spec helper

--- spec/acceptance/supportuser_spec.rb
+++ spec/acceptance/supportuser_spec.rb
@@ -3,6 +3,6 @@ describe 'tasks' do
   it 'task not to fail and password to be created ' do
     result = run_bolt_task('rsan::supportuser')
     #    expect(result['result']['_output']).to match('"status" : "created",')
-    expect(result[0]['status']).to eq('sucesss')
+    expect(result[0]['status']).to eq('0')
   end
 end
